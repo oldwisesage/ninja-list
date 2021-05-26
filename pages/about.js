5cea74e3238b8d6d1bbd36071d53b3bca5f2d5ed
oldwisesage <gandalf@Sages-MacBook-Pro-2.local>
2021-05-26 14:37:33 -0400

    getting styles config'd

---
+++ pages/about.js
@@ -0,0 +1,24 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quia
+        labore eum nam mollitia officia odio illo placeat nulla incidunt minus,
+        nobis nesciunt aut eos voluptate. Sit aspernatur eum deserunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quia
+        labore eum nam mollitia officia odio illo placeat nulla incidunt minus,
+        nobis nesciunt aut eos voluptate. Sit aspernatur eum deserunt.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quia
+        labore eum nam mollitia officia odio illo placeat nulla incidunt minus,
+        nobis nesciunt aut eos voluptate. Sit aspernatur eum deserunt.
+      </p>
+    </div>
+  );
+};
+
+export default About;
